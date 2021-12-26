51d4f44484ff350fa132a739e62d06f5c6c01b8c
MehmetMSezgin <mehmetmsezgin@gmail.com>
2021-12-26 20:44:21 +0200

    different branches feature b

---
+++ src/test/Java/FeatureBwork/FeatureB_class.java
@@ -0,0 +1,7 @@
+package test.Java.FeatureBwork;
+
+public class FeatureB_class {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/Java/FeatureWork/FeatureClass.java
@@ -0,0 +1,4 @@
+package test.Java.FeatureWork;
+
+public class FeatureClass {
+}
